61dfde228db967e0d02c1346b970044c341f0b3a
Connor Elkhill <connor.2.elkhill@cuanschutz.edu>
2023-11-02 08:52:53 -0600

    Adding the init file to the push

--- __init__.py
+++ __init__.py
@@ -5,5 +5,8 @@ CLASSIFIER_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), './Cr
 
 CRANIOFACIAL_LANDMARKING_MODEL_PATH = os.path.join(DATA_DIR, 'CraniofacialLandmarkingModel.dat')
 CRANIOFACIAL_LANDMARKING_NOTEXTURE_MODEL_PATH = os.path.join(DATA_DIR, 'CraniofacialLandmarkingModel-notexture.dat')
-GLABELLA_CRANIALBASE_LANDMARKS_PATH = os.path.join(DATA_DIR, 'landmarks_glabella_new.vtp')
-EURYON_CRANIALBASE_LANDMARKS_PATH = os.path.join(DATA_DIR, 'landmarks_full_templatespace.vtp')
+GLABELLA_CRANIALBASE_LANDMARKS_PATH = os.path.join(DATA_DIR, 'landmarks_ct_registration.vtp')
+EURYON_CRANIALBASE_LANDMARKS_PATH = os.path.join(DATA_DIR, 'landmarks_3dphoto_registration.vtp')
+CUTTING_LANDMARKS_PATH = os.path.join(DATA_DIR,'landmarks_for_cutting.vtp')
+
+TEMPLATE_VOLUME = 898.32
